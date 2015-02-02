b91071d3a23733a898fba7c32c951f41442bca71
FLKone <FLKone@users.noreply.github.com>
2015-02-02 04:41:10 +0100

    Better DnD with snapping/grid support. Multiple Buildings too.

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
 angular.module('biBuilder', [])
 .controller('MainCtrl', ['$scope', function($scope) {
 
-	$scope.gridSizeX = 25;
-	$scope.gridSizeY = 25;
+	$scope.gridSizeX = 20;
+	$scope.gridSizeY = 20;
 
-	$scope.gridX = 30;
-	$scope.gridY = 30;
+	$scope.gridX = 40;
+	$scope.gridY = 40;
 
 	$scope.totalX = $scope.gridSizeX * ($scope.gridX);
 	$scope.totalY = $scope.gridSizeY * ($scope.gridY);
@@ -49,12 +49,126 @@ angular.module('biBuilder', [])
 .directive('draggable', function($document) {
 return function(scope, element, attr) {
 
+	//referenciel #grid;
+	var refX = jQuery('#grid').offset().left;
+	var refY = jQuery('#grid').offset().top;
+
+	element.css({
+		position: 'relative',
+		border: '1px solid red',
+		backgroundColor: 'lightgrey',
+		cursor: 'pointer',
+		display: 'block',
+		width: scope.gridSizeX + 'px',
+		height: scope.gridSizeX + 'px'
+	});
+
+	element.on('mousedown', function(event) {
+		// Prevent default dragging of selected content
+		event.preventDefault();
+
+		//console.log ('screen ' + event.clientX + ' / ' + event.clientY);
+
+		var copie = $(element).clone();
+
+		copie.css({
+			position: 'absolute',
+			border: '1px solid green',
+			backgroundColor: 'lightgrey',
+			cursor: 'pointer',
+			display: 'block',
+			width: scope.gridSizeX * 2 + 'px',
+			height: scope.gridSizeX * 2 + 'px',
+			left: event.pageX - scope.gridSizeX/2 + 'px',
+			top: event.pageY - scope.gridSizeY/2 + 'px'
+		});
+
+		copie.appendTo('body');
+
+		function mousemove(event) {
+			y = event.pageY;
+			x = event.pageX;
+			//console.log(copie);
+			//console.log ('befor ' + x + ' x ' + y);
+
+			var caseX = Math.floor((x - refX) / scope.gridSizeX);
+			var caseY = Math.floor((y - refY) / scope.gridSizeY);
+			//console.log('caseY = ' + caseY + ' X ' + caseX);
+
+			//Snapping elmt to the grid
+			if (caseY >= 0 && caseX >= 0 && caseY < scope.gridY && caseX < scope.gridX) {
+				//console.log('inside the grid');
+
+				//console.log("y= " + y + " scope=" + scope.gridSizeY + ' mod=' + ( y % scope.gridSizeY ));
+				y = (caseY * scope.gridSizeY + refY);
+				x = (caseX * scope.gridSizeX + refX);
+			}
+			else {
+				//console.log('outside the grid');
+			}
+
+			//console.log ('after ' + x + ' / ' + y);
+			//console.log ('abspo ' + jQuery(element).offset().left + ' / ' + jQuery(element).offset().top);
+
+			copie.css({
+				top: y + 'px',
+				left: x  + 'px'
+			});
+		}
+
+		function mouseup(event) {
+			//console.log('mouseup');
+
+			$document.off('mousemove', mousemove);
+			$document.off('mouseup', mouseup);
+		}
+
+		function mousedown(event) {
+			event.preventDefault();
+
+			//console.log('mousedown');
+
+			$document.on('mousemove', mousemove);
+			$document.on('mouseup', mouseup);
+		}
+
+		$document.on('mousemove', mousemove);
+		copie.on('mousedown', mousedown);
+		$document.on('mouseup', mouseup);
+
+
+		//clonedElm = $(element).clone();
+		//console.log(clonedElm);
+/*
+		clonedElm.css({
+			position: 'absolute',
+			border: '1px solid green',
+			backgroundColor: 'lightgrey',
+			cursor: 'pointer',
+			display: 'block',
+			width: scope.gridSizeX + 'px',
+			height: scope.gridSizeX + 'px',
+			left: absX + 'px',
+			top: absY + 'px'
+		});
+*/
+		//$(event.srcElement).parent().append(clonedElm);
+
+//		clonedElm.on('mousemove', mousemove);
+//		clonedElm.on('mouseup', mouseup);
+	});
+/*
 	//start Values - used for relative mvmt
 	var startX = 0, startY = 0, x = 0, y = 0;
 
-	//base Values - used to replace elmt on deletion
-	var baseY = jQuery(element).offset().top;
-	var baseX = jQuery(element).offset().left;
+	//referenciel #grid;
+	var refY = jQuery('#grid').offset().top;
+	var refX = jQuery('#grid').offset().left;
+
+	var baseDiffX = refX - jQuery(element).offset().left;
+	var baseDiffY = refY - jQuery(element).offset().top;
+
+	console.log ('difff ' + baseDiffX + ' / ' + baseDiffY);
 
 	element.css({
 		position: 'relative',
@@ -79,20 +193,23 @@ return function(scope, element, attr) {
 		y = event.screenY - startY;
 		x = event.screenX - startX;
 
-		//referenciel #grid;
-		var refY = jQuery('#grid').offset().top;
-		var refX = jQuery('#grid').offset().left;
+		//console.log ('befor ' + x + ' x ' + y);
 
-		console.log ('refer ' + refX + ' / ' + refY);
-		console.log ('befor ' + x + ' / ' + y);
+		var absX = jQuery(element).offset().left;
+		var absY = jQuery(element).offset().top;
 
 		//Snapping elmt to the grid
-		if (x < scope.totalX && y < scope.totalY) {
-			y = y - ( y % scope.gridSizeY );
-			x = x - ( x % scope.gridSizeX );
+		if ((refX < absX && (refX + scope.totalX) > absX) && (refY < absY && (refY + scope.totalY) > absY) ) {
+			//console.log("y= " + y + " scope=" + scope.gridSizeY + ' mod=' + ( y % scope.gridSizeY ));
+			//y = y - ( y % scope.gridSizeY );
+			//x = x - ( x % scope.gridSizeX );
+		}
+		else {
+			//console.log('outside the grid');
 		}
 
-		console.log ('after ' + x + ' / ' + y);
+		//console.log ('after ' + x + ' / ' + y);
+		//console.log ('abspo ' + jQuery(element).offset().left + ' / ' + jQuery(element).offset().top);
 
 		element.css({
 			top: y + 'px',
@@ -106,23 +223,50 @@ return function(scope, element, attr) {
 		x = event.screenX - startX;
 		y = event.screenY - startY;
 
-		if (x > scope.totalX || y > scope.totalY) {
+
+		var absX = jQuery(element).offset().left;
+		var absY = jQuery(element).offset().top;
+
+		console.log ('befor ' + x + ' x ' + y);
+		console.log ('relat ' + absX + ' x ' + absY);
+		console.log ('refer ' + refX + ' / ' + refY);
+		console.log ('difff ' + baseDiffX + ' / ' + baseDiffY);
+
+
+
+		if ((refX < absX && (refX + scope.totalX) > absX) && (refY < absY && (refY + scope.totalY) > absY) )
+		{
+			//in  : OK;
+			console.log("y= " + y + " scope=" + scope.gridSizeY + ' mod=' + ( y % scope.gridSizeY ));
+			//y = y - ( y % scope.gridSizeY );
 
 			element.css({
-				top: baseY + 'px',
-				left:  baseX + 'px'
+				//top: ( y % scope.gridSizeY ) + 'px'
+				//left:  0 + 'px'
+			});
+		}
+		else {
+			//outside the grid : reseting elmt position.
+
+			element.css({
+				top: 0 + 'px',
+				left:  0 + 'px'
 			});
 
 			//reset elmt position if outside the grid;
-			startX = baseX;
-			startY = baseY;
+			startX = 0;
+			startY = 0;
 
-			x = baseX;
-			y = baseY;
+			x = 0;
+			y = 0;
+
 		}
-		
+
+
 		$document.off('mousemove', mousemove);
 		$document.off('mouseup', mouseup);
 	}
+
+	*/
 };
 });
